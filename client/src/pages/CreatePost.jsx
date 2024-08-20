8785a646c0d4af9a3b2d03ea9a2cc51f95cfb4c7
ampath <ampath@88e9fe78cca2.p.fiu.edu>
2024-08-20 10:03:56 -0400

    Removing the server

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if(form.prompt && form.photo) {
       setLoading(true)
       try {
-        const response = await fetch('https://dall-e-u0fk.onrender.com/api/v1/post', {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
@@ -53,7 +53,7 @@ const CreatePost = () => {
     if(form.prompt) {
       try {
         setgeneratingImg(true);
-        const response = await fetch('https://dall-e-u0fk.onrender.com/api/v1/dalle', {
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
